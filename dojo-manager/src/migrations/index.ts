64b92c33b425b76b0d279384eeda592eb731ae09
William Whorton <bwhorton@safetower.com>
2025-09-19 10:58:13 -0400

    Add migration for `20250919_145557` and update `importMap` formatting.

--- dojo-manager/src/migrations/index.ts
+++ dojo-manager/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250918_235319 from './20250918_235319';
+import * as migration_20250919_145557 from './20250919_145557';
 
 export const migrations = [
   {
     up: migration_20250918_235319.up,
     down: migration_20250918_235319.down,
-    name: '20250918_235319'
+    name: '20250918_235319',
+  },
+  {
+    up: migration_20250919_145557.up,
+    down: migration_20250919_145557.down,
+    name: '20250919_145557'
   },
 ];
